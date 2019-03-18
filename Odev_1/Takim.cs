28efc89d1024b55f2b06f90159ec64c01b8b8182
Hüseyin Emre Tutar <47827509+emretutar@users.noreply.github.com>
2019-03-18 20:18:55 +0300

    Update Takim.cs

--- Odev_1/Takim.cs
+++ Odev_1/Takim.cs
@@ -1,16 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Odev_1
 {
-    class Takim
+    class Team
     {
-        Asker[] birlik = new Asker[7];
+        Asker[] troops_1 = new Asker[7];
+        Asker[] troops_2 = new Asker[7];
+        public Asker[] Troops_1 { get { return troops_1; } set { troops_1 = value; } }
+        public Asker[] Troops_2 { get { return troops_2; } set { troops_2 = value; } }
 
-        public Asker[] Birlik { get { return birlik; } set { birlik = value; } }
+        //Determining troops
+        public Team()
+            {
+            //First 4 soldiers of Troops_1 are determining as Pvt.
+            for (int i=0; i<4; i++)
+                {
+                    this.Troops_1[i] = new Er();
+                }
+            //5th and 6th soldiers are determining as Lt.
+                this.Troops_1[4] = new Tegmen();
+                this.Troops_1[5] = new Tegmen();
+            //7th soldier is determining as Cpt.
+                this.Troops_1[6] = new Yuzbasi();
 
-        // ..... //
+                foreach(Asker item in Troops_1)
+                {
+                    item.Team = 1;
+                }
+
+            //First 4 soldiers of Troops_2 are determining as Pvt.
+            for (int i=0; i<4; i++)
+                {
+                    this.Troops_2[i] = new Er();
+                }
+            //5th and 6th soldiers are determining as Lt.
+                this.Troops_2[4] = new Tegmen();
+                this.Troops_2[5] = new Tegmen();
+            //7th soldier is determining as Cpt.
+                this.Troops_2[6] = new Yuzbasi();
+
+                foreach(Asker item in Troops_2)
+                {
+                    item.Team = 2;
+                }
+            }
+        }
     }
 }
