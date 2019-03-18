470cb0817560f1bf5be9557e77b1010c20d462dc
Hüseyin Emre Tutar <47827509+emretutar@users.noreply.github.com>
2019-03-18 20:21:35 +0300

    Update Asker.cs

--- Odev_1/Asker.cs
+++ Odev_1/Asker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,22 @@ namespace Odev_1
     abstract class Asker
     {
         private Bolge koordinat;
-        public Bolge Koordinat { get { return koordinat; } }
+        public Bolge Koordinat
+        {
+            get
+            {
+                return koordinat;
+            }
+        }
 
-        // ..... //
-
-        //Abstract sınıfların implementasyonları çoçuk sınıflarda gerçekleştirilmelidir.
+        public Ermeydani Movement_Control;
+        public bool Alive = true;
+        public int Life_Points = 100;
+        public int Damage;
+        public int Team;
+        
         public abstract void HaraketEt();
-
         public abstract void Bekle();
-
-        // ..... //
-
+        public abstract void AtesEt();
     }
 }
