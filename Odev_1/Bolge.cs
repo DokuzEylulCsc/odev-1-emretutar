04a995a47ce7171c5c2a9ad7a6ebda1ae190b638
Hüseyin Emre Tutar <47827509+emretutar@users.noreply.github.com>
2019-03-18 20:20:06 +0300

    Update Bolge.cs

--- Odev_1/Bolge.cs
+++ Odev_1/Bolge.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,13 @@ namespace Odev_1
 {
     class Bolge
     {
-        // ..... //
+        public int Coord_X, Coord_Y;
+
+        //Coordinates of x and y
+        public Bolge(int Coord_X, int Coord_Y)
+        {
+            this.Coord_X = Coord_X;
+            this.Coord_Y = Coord_Y;
+        }
     }
 }
