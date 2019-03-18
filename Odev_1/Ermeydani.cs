ad6e1bc3d4b525d9eee925976f7c6ed7b2d4b3a9
Hüseyin Emre Tutar <47827509+emretutar@users.noreply.github.com>
2019-03-18 20:17:42 +0300

    Update Ermeydani.cs

--- Odev_1/Ermeydani.cs
+++ Odev_1/Ermeydani.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,23 @@ namespace Odev_1
         Bolge[,] harita = new Bolge[16, 16];
 
         public Bolge[,] Harita { get { return harita; } set { harita = value; } }
+        //Accessing the troops
+        public Team Teams_of_War;
+        //Controlling the area
+        public bool Control;
 
-        // ..... //
+        //16 squares on the area are determining
+        public Ermeydani(Team Team_Square)
+        {
+            for(int i=0; i<16; i++)
+            {
+                for(int j=0; j<16; j++)
+                {
+                    Harita[i, j] = new Bolge(i,j);
+                }
+            }
+            Teams_of_War = Team_Square;
+        }
+
     }
 }
